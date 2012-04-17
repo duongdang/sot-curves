803f8a4c170bdde46b8558a021e683df98c7c48b
Duong Dang <nddang@laas.fr>
2012-04-17 17:11:30 +0200

    New prototype for nurbs. (Cloned from beziers).

---
+++ include/sot/curves/nurb.h
@@ -0,0 +1,62 @@
+// Copyright 2012 Duong Dang
+//
+// This file is part of sot-curves.
+// sot-curves is free software: you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+//
+// sot-curves is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Lesser Public License for more details.  You should have
+// received a copy of the GNU Lesser General Public License along with
+// sot-curves. If not, see <http://www.gnu.org/licenses/>.
+
+
+#ifndef __SOT_NURB_H__
+#define __SOT_NURB_H__
+# include <jrl/mal/boost.hh>
+namespace ml = maal::boost;
+
+/* SOT */
+#include <sot/core/flags.hh>
+#include <dynamic-graph/entity.h>
+#include <dynamic-graph/pool.h>
+#include <dynamic-graph/signal-ptr.h>
+#include <dynamic-graph/signal-time-dependent.h>
+#include <sot/core/exception-dynamic.hh>
+#include <sot/core/matrix-homogeneous.hh>
+
+namespace dynamicgraph
+{
+  namespace sot
+  {
+    namespace dg = dynamicgraph;
+    class Nurb
+  : public Entity
+{
+public:
+  static const std::string CLASS_NAME;
+  virtual const std::string& getClassName( void ) const { return CLASS_NAME; }
+  Nurb(const std::string &);
+
+  dg::SignalPtr<double, int> tSIN;
+  dg::SignalPtr<ml::Matrix, int> controlPointsSIN;
+
+  dg::SignalTimeDependent<ml::Vector ,int> stateSOUT;
+
+  ml::Vector& computeState(ml::Vector& res, int time);
+
+
+  void compute_nurb(maal::boost::Matrix Points, const double& t,
+                      maal::boost::Vector & res);
+
+};
+
+
+  }
+}
+
+
+#endif
---
+++ src/nurb.cc
@@ -0,0 +1,97 @@
+#include <cmath>
+#include <time.h>
+#ifndef WIN32
+# include <sys/time.h>
+#else
+# include <jrl/mal/boost.hh>
+# include <sot/core/utils-windows.hh>
+# include <Winsock2.h>
+#endif /*WIN32*/
+
+#include <sot/curves/nurb.h>
+#include <sot/core/debug.hh>
+#include <sot/core/macros-signal.hh>
+#include <dynamic-graph/factory.h>
+#include <fstream>
+
+#include <dynamic-graph/entity.h>
+
+namespace sot = dynamicgraph::sot;
+
+
+namespace dynamicgraph
+{
+  namespace sot
+  {
+    namespace{
+      void getLine(const ml::Matrix& m, int line_no,
+                   ml::Vector& res)
+      {
+        if (line_no < 0 || line_no > (int)m.nbRows())
+          {
+            return;
+          }
+        for (unsigned j = 0; j < m.nbCols(); j++)
+          {
+            res(j) = m.elementAt(line_no, j);
+          }
+
+      }
+    }
+
+
+    DYNAMICGRAPH_FACTORY_ENTITY_PLUGIN(Nurb,"Nurb");
+    Nurb::Nurb( const std::string & name )
+      : Entity(name),
+        tSIN(NULL,"sotNurb("+name+")::input(double)::t"),
+        controlPointsSIN(NULL,"sotNurb("+name+")::input(matrix)::controlPoints"),
+        stateSOUT( boost::bind(&Nurb::computeState,this,_1,_2),
+                   controlPointsSIN<<tSIN,
+                   "sotNurb("+name+")::output(vector)::state" )
+    {
+      sotDEBUGIN(5);
+      signalRegistration( controlPointsSIN<<tSIN<<stateSOUT);
+
+      sotDEBUGOUT(5);
+    }
+
+
+    void Nurb::compute_nurb(ml::Matrix points,
+                                const double& t, ml::Vector& result)
+    {
+      result.resize(points.nbCols());
+
+      // Recursive formula
+      if (points.nbRows() == 1)
+        {
+        getLine(points, 0, result);
+        return;
+        }
+      ml::Vector B_0_n1, B_1_n;
+      ml::Matrix points_0_n1 = points.extract(0,0, (int)points.nbRows() - 1,
+                                              (int)points.nbCols());
+      ml::Matrix points_1_n = points.extract(1,0,
+                                             (int)points.nbRows() - 1,
+                                             (int)points.nbCols());
+      compute_nurb(points_0_n1, t, B_0_n1);
+      compute_nurb(points_1_n , t, B_1_n);
+      result = (1 - t)*B_0_n1 + t*B_1_n;
+      return;
+    }
+
+    ml::Vector& Nurb::computeState(ml::Vector& res, int time)
+    {
+      ml::Matrix controlPoints = controlPointsSIN(time);
+      const double t = tSIN(time);
+      compute_nurb(controlPoints,
+                     t, res);
+      return res;
+
+    }
+
+
+
+  }
+}
+
+
